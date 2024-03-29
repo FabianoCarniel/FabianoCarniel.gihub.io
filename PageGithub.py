7f77bcf9e12eb32afa1f96608964d85cd8f2e05d
FabianoCarniel <69866172+FabianoCarniel@users.noreply.github.com>
2024-03-28 22:36:19 -0300

    Add files via upload

---
+++ PageGithub.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Surpresa</title>
+</head>
+<body>
+    <img src="https://github.com/FabianoCarniel/FabianoCarniel.gihub.io/raw/main/1.png" alt="Imagem Surpresa">
+</body>
+</html>
